09f8241c8231f589f2b0e064daa249062dbdfb57
krtuhin <tuhin.km2774@gmail.com>
2023-07-29 00:26:21 +0530

    Insert Operation

--- src/main/java/com/springboot/SpringBootWebApplication.java
+++ src/main/java/com/springboot/SpringBootWebApplication.java
@@ -1,13 +1,31 @@
 package com.springboot;
 
+import com.springboot.dao.UserRepository;
+import com.springboot.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        ApplicationContext context = SpringApplication.run(SpringBootWebApplication.class, args);
+
+        //get repository object from IoC container
+        UserRepository userRepository = context.getBean(UserRepository.class);
+
+        //create user object and set data
+        User user = new User();
+
+        user.setName("Tuhin Kumar");
+        user.setCity("Kolkata");
+
+        //save data into database
+        User user1 = userRepository.save(user);
+
+        System.out.println(user1);
+    }
 
 }
